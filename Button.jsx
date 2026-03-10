7f63d6d3639a9e0284660fe24d59c8a995785334
gatapro901-ops <gatapro901@gmail.com>
2026-03-10 10:22:51 +0300

    Empire Update: Button.jsx

---
+++ Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'>تسجيل الدخول</button>
+  );
+};
+
+export default Button;
